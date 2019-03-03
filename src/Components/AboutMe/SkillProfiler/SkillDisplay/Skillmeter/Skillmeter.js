ee7400c4a52ceb7dd76167c95b46f35f4f39ef37
Cancain <eriksson.tomas@gmail.com>
2019-03-03 20:10:08 +0100

    finsished the skillmeter

--- src/Components/AboutMe/SkillProfiler/SkillDisplay/Skillmeter/Skillmeter.js
+++ src/Components/AboutMe/SkillProfiler/SkillDisplay/Skillmeter/Skillmeter.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
 
+import style from './Skillmeter.module.css'
+
 class Skillmeter extends Component {
     state = {
         skillLevel: null
     }
 
+    skill = [false, false, false, false, false];
     componentDidMount() {
         this.setState({ skillLevel: this.props.level });
 
+        for (let i = 0; i < this.props.level; i++) {
+            this.skill[i] = true;
+        };
+        console.log(this.skill);
     }
 
 
     render() {
-        console.log(this.props.level);
-        const skillDisplay = (x) => {
-            for (let i = 0; i < x; i++) {
-                return <p>X</p>;
-            }
-        }
+
+        const skillDisplay = this.skill.map((skill, i) => {
+
+            let classes = [style.Bar]
+            skill ? classes.push(style.active) : classes.push(style.inactive)
+
+            return <div
+                className={classes.join(' ')}
+                key={i}></div>;
+        })
 
 
         return (
-            <div>
-                {skillDisplay(this.props.level)}
-
+            <div className={style.Skillmeter}>
+                {skillDisplay}
             </div>
         )
     }
--- src/Components/AboutMe/SkillProfiler/SkillProfiler.js
+++ src/Components/AboutMe/SkillProfiler/SkillProfiler.js
@@ -7,9 +7,26 @@ class SkillProfiler extends Component {
     state = {
         skills: [
             {
+                index: 7,
+                name: 'Basic Web',
+                level: 4,
+                subItems: [
+                    {
+                        index: '7a',
+                        name: 'HTML',
+                        level: 4
+                    },
+                    {
+                        index: '7b',
+                        name: 'CSS',
+                        level: 3
+                    }
+                ]
+            },
+            {
                 index: 0,
                 name: 'JavaScript',
-                level: 3,
+                level: 4,
                 subItems: [
                     {
                         index: '0a',
@@ -26,9 +43,90 @@ class SkillProfiler extends Component {
             {
                 index: 1,
                 name: 'PHP',
-                level: 4,
+                level: 3,
+                subItems: [
+                    {
+                        index: '1a',
+                        name: 'Wordpress',
+                        level: 2
+                    }
+                ]
+            },
+            {
+                index: 2,
+                name: 'C#',
+                level: 3,
+                subItems: [
+                    {
+                        index: '2a',
+                        name: 'LINQ',
+                        level: 1
+                    },
+                    {
+                        index: '2b',
+                        name: 'WPF',
+                        level: 1
+                    },
+                    {
+                        index: '2c',
+                        name: 'Xamarin',
+                        level: 1
+                    }
+                ]
+            },
+            {
+                index: 3,
+                name: 'Java',
+                level: 2,
+                subItems: [
+                    {
+                        index: '3a',
+                        name: 'JavaFx',
+                        level: 1
+                    },
+                    {
+                        index: '3b',
+                        name: 'Android development',
+                        level: 1
+                    }
+                ]
+            },
+            {
+                index: 4,
+                name: 'SQL',
+                level: 2,
+                subItems: [
+                    {
+                        index: '4a',
+                        name: 'PDO',
+                        level: 2
+                    }
+                ]
+            },
+            {
+                index: 5,
+                name: 'GIT',
+                level: 3,
                 subItems: []
+            },
+            {
+                index: 6,
+                name: 'English',
+                level: 4,
+                subItems: [
+                    {
+                        index: '6a',
+                        name: 'Written',
+                        level: 4
+                    },
+                    {
+                        index: '6b',
+                        name: 'Speaking',
+                        level: 4
+                    },
+                ]
             }
+
         ]
     }
 
