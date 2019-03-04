34a7e5682cd1e909ed5ef546c108ccb2c04e7217
Cancain <eriksson.tomas@gmail.com>
2019-03-04 12:10:28 +0100

    begun work on the portfolio page

--- src/Containers/Portfolio/Portfolio.js
+++ src/Containers/Portfolio/Portfolio.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import style from './Portfolio.module.css';
+
 const portfolio = (props) => {
     return (
-        <div>
-            <h1>Portfolio</h1>
+        <div className={style.Portfolio}>
+            <h1 >Portfolio</h1>
         </div>
     )
 }
