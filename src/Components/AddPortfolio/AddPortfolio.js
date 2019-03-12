b119cad55a4642979c54c60922fe2a1e7654c048
Cancain <eriksson.tomas@gmail.com>
2019-03-12 15:18:50 +0100

    fixed verification for addPortfolio

--- src/Components/AddPortfolio/AddPortfolio.js
+++ src/Components/AddPortfolio/AddPortfolio.js
@@ -9,7 +9,9 @@ class AddPortfolio extends Component {
         body: null,
         links: [
             {}
-        ]
+        ],
+        errMsg: null
+
     }
 
     addLinkHandler = () => {
@@ -20,42 +22,86 @@ class AddPortfolio extends Component {
         }
         links.push(emptyLink);
         this.setState({ links: links })
-        console.log(this.state.links)
     }
 
     saveLinkHandler = (index, event, isUrl) => {
         const links = this.state.links;
         let link;
 
-        isUrl ? link = { url: event.target.value } : link = { text: event.target.value }
+        isUrl ? link = { url: event.target.value, text: links[index].text } : link = { text: event.target.value, url: links[index].url }
         links[index] = link;
-
-        console.log(links[index]);
     }
 
+    verifyData = () => {
+        const title = this.state.title;
+        const body = this.state.body;
+        console.log('title is: ' + title);
+        console.log('body is: ' + body);
+        if (title !== null &&
+            body !== null) {
+            this.setState({ errMsg: null })
+            return true;
+        } else {
+            console.log('false');
+            this.setState({ errMsg: 'No field can be empty' })
+            return false;
+        }
+    }
+
+    submitHandler = () => {
+        if (this.verifyData()) {
+            const data = this.state;
+            console.log(data);
+        }
+    }
 
     render() {
-        console.log('length is: ' + this.state.links.length);
-
         const linkInputHandler = this.state.links.map((link, index) => {
             return <div key={index}>
-                <input onChange={(event) => this.saveLinkHandler(index, event, true)} placeholder="URL here... "></input>
-                <input onChange={(event) => this.saveLinkHandler(index, event, false)} placeholder="Text here..."></input>
+                <input
+                    onChange={(event) => this.saveLinkHandler(index, event, true)}
+                    placeholder="URL here... ">
+                </input>
+
+                <input
+                    onChange={(event) => this.saveLinkHandler(index, event, false)}
+                    placeholder="Text here...">
+                </input>
+
             </div>
         });
 
         return (
             <div className={style.AddPortfolio}>
-                <input type="text"
-                    placeholder="Title goes here"></input>
-                <textarea placeholder="Body goes here..."></textarea>
+                <input
+                    type="text"
+                    placeholder="Title goes here"
+                    onChange={(event) => this.setState({ title: event.target.value })}
+                >
+                </input>
+
+                <textarea
+                    placeholder="Body goes here..."
+                    onChange={(event) => this.setState({ body: event.target.value })}
+                >
+                </textarea>
+
                 <button
                     onClick={this.addLinkHandler}
                 >
                     Add link
                 </button>
+
                 {linkInputHandler}
-                <input type="button" value="Submit"></input>
+
+                <input
+                    type="button"
+                    value="Submit"
+                    onClick={this.submitHandler}
+                >
+                </input>
+                {this.state.errMsg ? <span>{this.state.errMsg}</span> : null}
+
             </div>
         )
     }
