a0b08352edfceaf0142e80e39f3c7c148270a29c
Cancain <eriksson.tomas@gmail.com>
2019-03-13 11:38:17 +0100

    fronend for contact done

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -4,12 +4,15 @@ import style from './Contact.module.css';
 
 const contact = (props) => {
     return (
-        <div>
+        <div className={style.Contact}>
             <h1>Contact me</h1>
-            <label>Email:</label>
-            <input placeholder="Your email here..." type="text"></input>
-            <label>Message:</label>
-            <textarea placeholder="Your message here..."></textarea>
+            <div className={style.Wrapper}>
+                <label>Email:</label>
+                <input placeholder="Your email here..." type="text"></input>
+                <label>Message:</label>
+                <textarea placeholder="Your message here..."></textarea>
+                <button>Send mail</button>
+            </div>
         </div >
     )
 }
